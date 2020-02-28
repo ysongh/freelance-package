049077babd4f5f2b5f9384ad9cf17c60bf15b1d5
You Song Hou <yousonghou@yahoo.com>
2020-02-27 23:23:21 -0500

    move the navbar to the top when the top bar is hidden

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,16 @@
 const topBar = document.querySelector('.top-bar');
+const desk = document.querySelector('.desk');
+const mobileNav = document.querySelector('.mobileNav');
 
 window.addEventListener('scroll', function (e) {
     if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
         topBar.classList.add('u-hide');
+        desk.classList.add('u-top-0');
+        mobileNav.classList.add('u-top-0');
     }
     else {
         topBar.classList.remove('u-hide');
+        desk.classList.remove('u-top-0');
+        mobileNav.classList.remove('u-top-0');
     }
 });
