d65383afc0c56fe29e072f015c0e7abbc34d3700
You Song Hou <yousonghou@yahoo.com>
2020-03-23 19:11:29 -0400

    move jquery code to navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -14,3 +14,30 @@ window.addEventListener('scroll', function (e) {
         mobileNav.classList.remove('u-top-0');
     }
 });
+
+$(document).ready(function () {
+    $('.smooth-goto').on('click', function () {
+        $('html, body').animate({ scrollTop: $(this.hash).offset().top - 150}, 0);
+        return false;
+    });
+
+    $(".hamburger-btn .fa-times").hide();
+
+    $(".hamburger-btn .fa-bars").click(function () {
+        $(this).hide();
+        $(".hamburger-btn .fa-times").show();
+        $(".mobileNav ul").addClass("active");
+    });
+
+    $(".hamburger-btn .fa-times").click(function () {
+        $(this).hide();
+        $(".hamburger-btn .fa-bars").show();
+        $(".mobileNav ul").removeClass("active");
+    });
+
+    $(".mobileNav_link").click(function () {
+        $(".hamburger-btn .fa-times").hide();
+        $(".hamburger-btn .fa-bars").show();
+        $(".mobileNav ul").removeClass("active");
+    });
+});
